d53c00f4dba09422c58e26924a146e51304c7f10
krisitown <krisipwn@gmail.com>
2019-08-01 02:55:12 +0300

    added validations for rssUrl model

--- app/models/rss_url.rb
+++ app/models/rss_url.rb
@@ -1,2 +1,3 @@
 class RssUrl < ApplicationRecord
+    validates :url, presence: true, length: {minimum: 10, maximum: 255}
 end
--- spec/model/rss_url_spec.rb
+++ spec/model/rss_url_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe RssUrl, type: :model do
     context "url is a blank string" do
         it "is an invalid model" do
             rssUrl = RssUrl.create({url: ""})
-            expect(rssUrl).to eq(false)
+            expect(rssUrl.valid?).to eq(false)
         end
     end
 
     context "url is a valid url" do
         it "creates a valid model" do
             rssUrl = RssUrl.create({url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"})
-            expect(rssUrl).to eq(true)
+            expect(rssUrl.valid?).to eq(true)
         end
     end
 end
