6a642a802aeeacb07acf9389fbfc2e3c88943d93
krisitown <krisipwn@gmail.com>
2019-08-01 02:52:47 +0300

    wrote spec for rssUrl model, needed to make some changes to gemfile versions

---
+++ spec/model/rss_url_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe RssUrl, type: :model do
+    context "url is a blank string" do
+        it "is an invalid model" do
+            rssUrl = RssUrl.create({url: ""})
+            expect(rssUrl).to eq(false)
+        end
+    end
+
+    context "url is a valid url" do
+        it "creates a valid model" do
+            rssUrl = RssUrl.create({url: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"})
+            expect(rssUrl).to eq(true)
+        end
+    end
+end
